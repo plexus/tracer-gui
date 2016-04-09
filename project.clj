2fe442c7c7a7387abc68d3fd8e7857647da40012
Arne Brasseur <arne@arnebrasseur.net>
2016-04-09 19:49:40 +0200

    Compile with Clojure 1.7 for backwards compatibility

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/plexus"
   :license {:name "Mozilla Public License 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :plugins []
   :source-paths ["src"]
-  :test-paths ["test"])
+  :test-paths ["test"]
+  :aot [tracer-gui.gui])
